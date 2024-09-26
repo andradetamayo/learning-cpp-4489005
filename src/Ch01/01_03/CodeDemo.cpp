f043727f3cfa0c54408955562f7e6366e37120a6
David Andrade <62023685+andradetamayo@users.noreply.github.com>
2024-09-26 20:20:39 +0000

    fixed input request

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -7,10 +7,11 @@
 
 int main(){
     std::string str;
+    std::cout <<"Enter your name: " << std::flush;
     std::cin >> str;
     //std::cout << str;
 
-    std::cout << "Hi there, " <<str + "!" << std::endl;
+    std::cout << "Hi there, " << str << "!" << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
